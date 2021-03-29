f6bb88a9be510f0012ae8ed0d5592af52a7873f9
inbochoi <inbo207@naver.com>
2021-03-29 20:38:13 +0200

    First change

--- AlgoStudy/Program.cs
+++ AlgoStudy/Program.cs
@@ -7,6 +7,7 @@ namespace AlgoStudy
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("first change");
         }
     }
 }
